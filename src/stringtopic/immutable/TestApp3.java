0235def86289ddb1423561d3d445312aef05ece1
rahul kirpekar <rahul.kirpekar@gmail.com>
2025-11-19 08:51:04 +0530

    string methods done

--- src/stringtopic/immutable/TestApp3.java
+++ src/stringtopic/immutable/TestApp3.java
@@ -7,6 +7,12 @@ public class TestApp3
 		//			    0   1    2    3    4   5       6   7      8
 		String stmt = "This is Java which is developed By James Gosling";
 		
+		System.out.println(stmt);
+		
+		String a = stmt.repeat(5);
+		
+		System.out.println(a);
+		
 		// String convert into bytes
 //		byte b[] = stmt.getBytes();
 //		for (int i = 0; i < b.length; i++) 
