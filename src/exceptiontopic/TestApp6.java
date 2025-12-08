41b1a0d01c46b3c3511defb91f5abaaa3dd894a8
rahul kirpekar <rahul.kirpekar@gmail.com>
2025-12-08 09:03:28 +0530

    throw and throws

---
+++ src/exceptiontopic/TestApp6.java
@@ -0,0 +1,41 @@
+package exceptiontopic;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TestApp6 
+{
+	public static void isValidForVote(int age) throws ArithmeticException
+	{
+		if (age < 18) 
+		{
+			// raise exception---custom Exception
+			// 1. checked exception
+//			throw new IOException("\nInvalid Age,\n\tPlease netr valid age for Vote");
+			
+			// 2. Unchecked exception
+			throw new ArithmeticException("\nInvalid Age,\n\tPlease netr valid age for Vote");
+		} 
+		else 
+		{
+			System.out.println("Welcome for Vote");
+		}
+	}
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Age for Votting : ");
+		int age = sc.nextInt();
+		
+		try 
+		{
+			isValidForVote(age);
+		} 
+		catch (ArithmeticException e) 
+		{
+			e.printStackTrace();
+		}
+		System.out.println("=======General Statement=======");
+	}
+}
